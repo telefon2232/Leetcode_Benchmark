180da7538adae1da10f5b614cd0573da02d36cb1
mblack <telefon2232@gmail.com>
2023-04-01 00:30:39 +0300

    options

--- gotest.sh
+++ gotest.sh
@@ -5,5 +5,5 @@ set -e
 for d in $(go list ./leetcode/... | grep -v vendor); do
     echo $d
     go test -benchtime 1x -bench .     $d | grep ns 
-   
+   # go test -c -o foo  -> ./foo -test.bench .
 done
