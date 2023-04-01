bfd4b871ff7edf04520a59b7d022fbc0ca2e59dc
mblack <telefon2232@gmail.com>
2023-04-01 18:01:00 +0300

    add compile

---
+++ compile.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+ARCH=${1} # only riscv64, arm64, amd64
+
+NAMES=`ls ./leetcode`
+
+#echo $NAMES
+
+mkdir -p $ARCH
+
+for NAME in $NAMES
+do
+
+cd ./leetcode/$NAME
+GOARCH=${ARCH} go test -c -o $NAME
+mv $NAME ../../${ARCH}
+echo $NAME compiled!
+cd ../..
+
+
+done
--- gotest.sh
+++ gotest.sh
@@ -5,7 +5,7 @@ set -e
 for d in $(go list ./leetcode/... | grep -v vendor); do
     echo $d
     go test -benchtime 1x -bench .  $d | grep ns 
-    # go test -c -o foo  -> ./foo -test.bench .
+    # go test -c -o foo  #-> ./foo -test.bench .
     # GOARCH=arm go test -c -o foo
     # GOARCH=riscv64 go test -c -o foo
     # go tool pprof -top  cpu.out
