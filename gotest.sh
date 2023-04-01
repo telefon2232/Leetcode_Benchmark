637dd008f40efb1ab5e92c697ff63c7de5ff441f
mblack <telefon2232@gmail.com>
2023-04-01 19:43:28 +0300

    add print

--- gotest.sh
+++ gotest.sh
@@ -5,8 +5,11 @@ set -e
 for d in $(go list ./leetcode/... | grep -v vendor); do
     echo $d
     go test -benchtime 1x -bench .  $d | grep ns 
+
+
     # go test -c -o foo  #-> ./foo -test.bench .
     # GOARCH=arm go test -c -o foo
     # GOARCH=riscv64 go test -c -o foo
     # go tool pprof -top  cpu.out
 done
+echo "Finished `ls ./leetcode/ | wc -l` tests!"
