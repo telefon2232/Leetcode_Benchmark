3033b0d5e14d2c1d062e78dfd1c250eadf4e84da
mblack <telefon2232@gmail.com>
2023-04-01 17:04:07 +0300

    remove useless and add usefull

--- gotest.sh
+++ gotest.sh
@@ -4,7 +4,7 @@ set -e
 
 for d in $(go list ./leetcode/... | grep -v vendor); do
     echo $d
-    go test -benchtime 10x -bench .     $d | grep ns 
+    go test -benchtime 1s -bench .     $d | grep ns 
     # go test -c -o foo  -> ./foo -test.bench .
     # GOARCH=arm go test -c -o foo
     # GOARCH=riscv64 go test -c -o foo
